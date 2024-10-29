bddf66e6bc3e53855051daf506ed7adefdf05920
Hadole Raju <67407713+rajureddyhadole@users.noreply.github.com>
2024-10-29 19:07:52 +0530

    add edit option

--- app.js
+++ app.js
@@ -28,6 +28,17 @@ app.post("/create", async (req, res) => {
   res.redirect('/read');
 });
 
+app.get("/edit/:userid", async (req, res) => {
+  let user = await userModel.findOne({_id: req.params.userid});
+  res.render("edit", {user});
+});
+
+app.post("/update/:userid", async (req, res) => {
+  let {name, email, image} = req.body;
+  let user = await userModel.findOneAndUpdate({_id: req.params.userid}, {name, email, image}, {new: true});
+  res.redirect('/read');
+});
+
 app.get("/delete/:id", async (req, res) => {
   let users = await userModel.findOneAndDelete({_id: req.params.id});
   res.redirect('/read');
